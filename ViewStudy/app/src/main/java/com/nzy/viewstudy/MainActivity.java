c40921a2085c5320fbc552d766c824cfe9a432f2
niezhiyang <nie.zhiyang@immomo.com>
2020-09-01 15:26:13 +0800

    自定义View

--- ViewStudy/app/src/main/java/com/nzy/viewstudy/MainActivity.java
+++ ViewStudy/app/src/main/java/com/nzy/viewstudy/MainActivity.java
@@ -2,6 +2,7 @@ package com.nzy.viewstudy;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.nzy.viewstudy.view.FlowLayout;
 
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     FlowLayout flowLayout;
     View myview;
     private int  Lenth = 100000;
-
+    private TextView tv_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         flowLayout = findViewById(R.id.iv_flowlayougt);
         myview = findViewById(R.id.myview);
+
+        tv_name = findViewById(R.id.tv_name);
+        tv_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    @Override
+                    public void run() {
+                        super.run();
+                        try {
+                            tv_name.setText("你倒是更新呀");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }.start();
+            }
+        });
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    tv_name.setText("子线程更新了");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
 //        new Handler().postDelayed(new Runnable() {
 //            @Override
 //            public void run() {
@@ -31,4 +64,20 @@ public class MainActivity extends AppCompatActivity {
 //        },6);
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+
 }
--- ViewStudy/webviewnew/src/main/java/com/nzy/webviewnew/webview/WebViewActivity.java
+++ ViewStudy/webviewnew/src/main/java/com/nzy/webviewnew/webview/WebViewActivity.java
@@ -1,12 +1,16 @@
 package com.nzy.webviewnew.webview;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.View;
 
 import com.nzy.webviewnew.R;
 import com.nzy.webviewnew.databinding.ActivityWebviewBinding;
 import com.nzy.webviewnew.webview.utils.Constants;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -17,15 +21,41 @@ import androidx.fragment.app.FragmentTransaction;
 public class WebViewActivity extends AppCompatActivity {
     private ActivityWebviewBinding mBinding;
 
-    public static  WebViewActivity activity ;
+    public static WebViewActivity activity;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                Handler handler = new Handler(Looper.getMainLooper()) {
+                    @Override
+                    public void handleMessage(@NonNull Message msg) {
+                        super.handleMessage(msg);
+                    }
+                };
+            }
+        }.start();
+//        new Thread(new Runnable() {
+//            public void run() {
+//                Handler handler = new Handler() {
+//                    @Override
+//                    public void handleMessage(Message msg) {
+//                        Toast.makeText(getApplicationContext(), "handler msg", Toast.LENGTH_LONG).show();
+//                    }
+//                };
+//                handler.sendEmptyMessage(1);
+//
+//            }
+//
+//            ;
+//        }).start();
         activity = this;
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_webview);
         mBinding.title.setText(getIntent().getStringExtra(Constants.TITLE));
-        mBinding.actionBar.setVisibility(getIntent().getBooleanExtra(Constants.IS_SHOW_ACTION_BAR, true)? View.VISIBLE:View.GONE);
+        mBinding.actionBar.setVisibility(getIntent().getBooleanExtra(Constants.IS_SHOW_ACTION_BAR, true) ? View.VISIBLE : View.GONE);
         mBinding.back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,7 +69,7 @@ public class WebViewActivity extends AppCompatActivity {
 
     }
 
-    public void updateTitle(String title){
+    public void updateTitle(String title) {
         mBinding.title.setText(title);
     }
 }
